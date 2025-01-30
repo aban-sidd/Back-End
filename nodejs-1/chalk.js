be1a12625e71cd04cce7e174d2e7937b8548bb04
Muhammad Aban Siddiqui <abansiddiqui4@gmail.com>
2025-01-30 13:19:39 +0500

    Removed all files from GitHub

--- nodejs-1/chalk.js
+++
@@ -1,8 +0,0 @@
-const chalk = require('chalk')
-
-const {name , age , city} = require('./import')
-
-console.log(chalk.bold.red(name))
-console.log(chalk.green(age))
-console.log(chalk.bold.blue(city))
-
